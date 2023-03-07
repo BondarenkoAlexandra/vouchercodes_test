d162c7355228136d88723c7413b87728bf1f612e
Alexandra Bondarenko <bondarenko21196@gmail.com>
2023-03-07 02:12:57 +0000

    fixed the ID

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '9soo6c',
+  projectId: 'jpq9zw',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
