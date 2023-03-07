955f6844aae3e641f0b1836067b060ec5a77c118
Alexandra Bondarenko <bondarenko21196@gmail.com>
2023-03-07 02:46:10 +0000

    deleted example.json

--- cypress/e2e/vouchercodes_test.cy.js
+++ cypress/e2e/vouchercodes_test.cy.js
@@ -1,27 +1,26 @@
 /// <reference types="cypress"/>
 
-describe("Search offers", function(){
+describe("Search offers", function () {
 
-  it("Search restaurant in London",()=>{
-      cy.visit("https://www.vouchercodes.co.uk/restaurant-vouchers.html")
-      cy.get('[data-qa="el:restaurantHeading"]').should ("contain.text", "Find restaurant vouchers & offers near you");
-      cy.get('[data-qa="el:locationDropdown enabled:true"]')
-      .type('London', {delay:300})
+  it("Search restaurant in London", () => {
+    cy.visit("https://www.vouchercodes.co.uk/restaurant-vouchers.html")
+    cy.get('[data-qa="el:restaurantHeading"]').should("contain.text", "Find restaurant vouchers & offers near you");
+    cy.get('[data-qa="el:locationDropdown enabled:true"]')
+      .type('London', { delay: 300 })
       .type('{downArrow}')
       .type('{enter}');
-      cy.get('[data-qa="el:dayDropdown"]').select('');
-      cy.get('[data-qa="el:peopleDropdown"]').select('2');
-      cy.get('[data-qa="el:findRestaurantsVoucherButton"]').click();
+    cy.get('[data-qa="el:dayDropdown"]').select('');
+    cy.get('[data-qa="el:peopleDropdown"]').select('2');
+    cy.get('[data-qa="el:findRestaurantsVoucherButton"]').click();
 
-      // next steps it is an assumption that there are no results(expected result)
-      cy.get('[data-qa="el:infoBannerTitle"]').should ("contain.text", "Sorry, we couldn't find any offers that match your criteria");
-})
-  
-it("Get student sign-in page",()=>{
-      cy.visit("https://www.vouchercodes.co.uk/")
-      cy.get('[data-qa="el:studentsHeader active:false"]').click();
-      cy.get('[data-qa="el:slideUrl link:/campaign/student-sign-up"]').click();
-      cy.url().should('include','/campaign/student-sign-in');
-})
+    // next steps it is an assumption that there are no results(expected result)
+    cy.get('[data-qa="el:infoBannerTitle"]').should("contain.text", "Sorry, we couldn't find any offers that match your criteria");
+  })
 
+  it("Get student sign-in page", () => {
+    cy.visit("https://www.vouchercodes.co.uk/")
+    cy.get('[data-qa="el:studentsHeader active:false"]').click();
+    cy.get('[data-qa="el:slideUrl link:/campaign/student-sign-up"]').click();
+    cy.url().should('include', '/campaign/student-sign-in');
+  })
 })
